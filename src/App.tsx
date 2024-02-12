67572986bc5bdaebb5955301632dba115f500581
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-12 22:59:40 +0100

    fix: ContextProvider wraps App.tsx correctly in index.tsx now

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 import { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ContextProvider, AppContext } from "./ContextProvider";
+import { AppContext } from "./ContextProvider";
 
 function App() {
 	const { isAuthenticated, toggleIsAuthenticated } = useContext(AppContext);
 
-	// DEBUG : no syntax error, yet `toggleIsAuthenticated()` is not working.
-	console.log(isAuthenticated);
-	console.log(toggleIsAuthenticated);
-
 	return (
 		<BrowserRouter>
-			<ContextProvider>
-				<button
-					onClick={() => {
-						console.log("Testing this click");
-						toggleIsAuthenticated();
-					}}
-				>
-					Click
-				</button>
-				{!isAuthenticated ? <button>Login</button> : "Let's see the `/home`"}
-				<Routes>
-					<Route path="/">Home</Route>
-					<Route path="/:id">Detail</Route>
-				</Routes>
-			</ContextProvider>
+			<button onClick={toggleIsAuthenticated}>Click</button>
+			{!isAuthenticated ? <button>Login</button> : "Let's see the `/home`"}
+			<Routes>
+				<Route path="/">Home</Route>
+				<Route path="/:id">Detail</Route>
+			</Routes>
 		</BrowserRouter>
 	);
 }
