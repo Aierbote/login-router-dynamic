b6daa8bbd5c0fd7c7718563638da6a814cd704ea
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-13 21:18:25 +0100

    feat: preliminary DetailRoute component
    working with `useParams` hook and `:idPost`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 import { LoginRoute } from "./components/LoginRoute";
 import { HomeRoute } from "./components/HomeRoute";
 import { MyNavbar } from "./components/MyNavbar";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+// import { useState } from "react";
+
+function DetailRoute() {
+	const { idPost } = useParams();
+
+	return (
+		<div>
+			<h1>Draft Post Details {idPost}</h1>
+		</div>
+	);
+}
 
 function App() {
 	// const { isAuthenticated } = useContext(AppContext);
@@ -19,7 +30,7 @@ function App() {
 					<Route path="/" element={<HomeRoute />} />
 				</Route>
 				<Route element={<ProtectedRoute />}>
-					<Route path="/:id">Detail</Route>
+					<Route path="/:idPost" element={<DetailRoute />} />
 				</Route>
 				{/* // NO SYNTAX ERROR, but `Routes` cannot render non-`Route` elements */}
 				{/* <ProtectedRoute>
