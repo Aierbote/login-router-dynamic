1af8d3b2bdaf6e27d41047f373ba02cd09bd270d
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-14 03:41:49 +0100

    feat: add getPostCommentsDetails in useFetch
    renamed usePostFetch as useFetch

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,43 @@ import { ProtectedRoute } from "./components/ProtectedRoute";
 import { useEffect, useState } from "react";
 // import { useState } from "react";
 
-function usePostFetch(idPost: string) {
+function useFetch(idPost?: string) {
 	// const initPostData = !!localStorage.getItem("postData")
 	// 	? JSON.parse(localStorage.getItem("postData") as string)
 	// 	: null;
 
-	const [postData, setPostData] = useState<{
+	interface PostDataType {
 		userId: string;
 		id: string;
 		title: string;
 		body: string;
-	}>({ userId: "", id: "", title: "", body: "" });
+	}
 
-	const getSingleDetail = async () => {
+	interface CommentDataType {
+		postId: string;
+		id: string;
+		name: string;
+		email: string;
+		body: string;
+	}
+
+	const [postData, setPostData] = useState<PostDataType>({
+		userId: "",
+		id: "",
+		title: "",
+		body: "",
+	});
+	const [commentsData, setCommentsData] = useState<Array<CommentDataType>>([
+		{
+			postId: "",
+			id: "",
+			name: "",
+			email: "",
+			body: "",
+		},
+	]);
+
+	const getPostDetails = async () => {
 		try {
 			const response = await fetch(
 				`https://jsonplaceholder.typicode.com/posts/${idPost}`
@@ -28,26 +52,57 @@ function usePostFetch(idPost: string) {
 			setPostData(data);
 			console.log("data", data);
 		} catch (err: any) {
-			console.error(`Something Wrong: ${err}`);
+			console.error(`Something Wrong with fetching a post with idPost: ${err}`);
+		}
+	};
+
+	const getPostCommentsDetails = async () => {
+		try {
+			const response = await fetch(
+				`https://jsonplaceholder.typicode.com/comments`
+			);
+			const data = await response.json();
+
+			const filteredComments = data.filter(
+				(comment: CommentDataType) => "" + comment.postId === idPost // NOTE : postId is int in API, while idPost is string
+			);
+
+			setCommentsData(filteredComments);
+		} catch (err) {
+			console.error(`Something Wrong with fetching all comments': ${err}`);
 		}
 	};
 
 	useEffect(() => {
-		getSingleDetail();
+		getPostDetails();
+		getPostCommentsDetails();
 	}, []);
 
-	return [postData];
+	return { postData, commentsData };
 }
 
 function DetailRoute() {
 	const { idPost } = useParams();
-	const [postData] = usePostFetch(idPost as string);
+	const { postData, commentsData } = useFetch(idPost as string);
 
 	return (
 		<div>
-			<h1>Draft Post Details {idPost}</h1>
-			<h3>{postData.title.toUpperCase()}</h3>
-			<p>{postData.body}</p>
+			<div className="post-card">
+				<h1>Draft Post Details {idPost}</h1>
+				<h3>{postData.title.toUpperCase()}</h3>
+				<p>{postData.body}</p>
+			</div>
+			<div className="comments-list">
+				<h3>Comments</h3>
+				<ul>
+					{commentsData.map((comment) => (
+						<li key={comment.id}>
+							<h4>{comment.email}</h4>
+							<p>{comment.body}</p>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
