4a64c9639c97ad24fcb3398880d32f95b2da935e
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-14 01:00:01 +0100

    feat: add preliminary getSingleDetail fn to fetch API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import {
+	BrowserRouter,
+	Navigate,
+	Route,
+	Routes,
+	useParams,
+} from "react-router-dom";
 import { LoginRoute } from "./components/LoginRoute";
 import { HomeRoute } from "./components/HomeRoute";
 import { MyNavbar } from "./components/MyNavbar";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 // import { useState } from "react";
+
+function usePostsList() {
+	const getSingleDetail = async () => {
+		try {
+			const response = await fetch(
+				`https://jsonplaceholder.typicode.com/posts/${idPost}`
+			);
+			const data = await response.json();
+
+			return data;
+		} catch (err: any) {
+			console.error(`Something Wrong: ${err}`);
+
+			return null;
+		}
+	};
+
+	const singleDetail = getSingleDetail();
+}
 
 function DetailRoute() {
 	const { idPost } = useParams();
